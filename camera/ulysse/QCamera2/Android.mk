6cdedd94fe471c7b64cb55449750c596429f1019
Bruno Martins <bgcngm@gmail.com>
2020-11-28 20:45:34 +0400

    mi8937: camera: ulysse: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.
    
    Change-Id: I85d229e536146b86662e9c9a28017d2448c856dd
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- camera/ulysse/QCamera2/Android.mk
+++ camera/ulysse/QCamera2/Android.mk
@@ -93,6 +93,7 @@ LOCAL_HEADER_LIBRARIES += libcutils_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_HEADER_LIBRARIES += ulysse_camera_common_headers
+LOCAL_HEADER_LIBRARIES += display_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
@@ -119,8 +120,6 @@ ifeq ($(TARGET_KERNEL_VERSION),4.9)
 endif
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
-LOCAL_C_INCLUDES += \
-        $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
