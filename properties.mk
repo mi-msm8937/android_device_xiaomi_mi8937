dfccb5295b81cd74575666704aa199355d982572
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-22 10:53:40 +0100

    mi8937: Disable CDS via property
    Change-Id: I5fc30d41301ac6669c79154ba3d945b66adb7c21

--- properties.mk
+++ properties.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.camera.CDS=off \
+persist.vendor.camera.video.CDS=off
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.lcd_density=267
