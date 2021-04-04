f951f636500b85d45f18cf278776d2dd7d2ebfab
zqm64 <zqm64@outlook.com>
2021-04-04 23:52:54 +0800

    mi8937: biometrics: Import FP HIDL from lineage-18.1
    HEAD is at 297cf8975e647eea7a6b75d5ee0ecd5e70880037

--- biometrics/product.mk
+++ biometrics/product.mk
@@ -4,3 +4,8 @@ DEVICE_PACKAGE_OVERLAYS += device/xiaomi/mi8937/biometrics/overlay-lineage
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mi8937 \
+    android.hardware.biometrics.fingerprint@2.2
