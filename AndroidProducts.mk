5a409d26fbeb836a28ee26cac388eb276c42c115
zqm64 <zqm64@outlook.com>
2021-04-19 22:38:41 +0800

    mi8937: CherishOS Bringup
    Please track vendor/qcom/opensource/interfaces from LOS otherwise vendor.qti.hardware.perf@2.0 would be missing.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_mi8937-user \
     lineage_mi8937-userdebug \
     lineage_mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cherish_mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    cherish_mi8937-user \
+    cherish_mi8937-userdebug \
+    cherish_mi8937-eng
