4617fb939a1a859220cc6c09903dbfc4e4df29df
zqm64 <zqm64@outlook.com>
2021-05-13 19:49:55 +0800

    mi8937: Import IR from msm8937-common

--- device.mk
+++ device.mk
@@ -11,6 +11,15 @@ $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
+# Consumer IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
