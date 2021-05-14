bab94ef1cbe0a01882caca969bd807ae24923d40
zqm64 <zqm64@outlook.com>
2021-05-14 23:21:29 +0800

    mi8937: Import gatekeeper HIDL from msm8937-common

--- device.mk
+++ device.mk
@@ -20,6 +20,11 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
