822c5b74aab4e64d0a656073e54e567724938228
zqm64 <zqm64@outlook.com>
2021-05-20 19:14:14 +0800

    mi8937: biometrics: Shim libbinder dependency with libgxbinder

---
+++ libshims/libgxbinder.cpp
@@ -0,0 +1,16 @@
+#include <binder/ProcessState.h>
+
+namespace android {
+sp<ProcessState> ProcessState::initWithDriver(const char* driver)
+{
+    return nullptr;
+}
+sp<ProcessState> ProcessState::self()
+{
+    return nullptr;
+}
+void ProcessState::startThreadPool()
+{
+    return;
+}
+} // namespace android
