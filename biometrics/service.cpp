ddde1b27bc5cab1db3d22c71376e976c21caca5d
zqm64 <zqm64@outlook.com>
2021-05-22 14:24:37 +0800

    Revert "mi8937: biometrics: Start threadpool on vndbinder"
    This reverts commit 1e4bb340869b4882dab318dfc581ba44777c1258.

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -42,8 +42,8 @@ int main() {
     android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
 
     if (is_old_goodix) {
-        // the conventional HAL might start vndbinder services
-        android::ProcessState::initWithDriver("/dev/vndbinder");
+        // the conventional HAL might start binder services
+        android::ProcessState::initWithDriver("/dev/binder");
         android::ProcessState::self()->startThreadPool();
     }
 
