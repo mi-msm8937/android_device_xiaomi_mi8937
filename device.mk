5b422b0a452999355807a762905e6c36d9713b38
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-08 21:40:34 +0800

    mi8937: Enable QTI Vibrator AIDL

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Vibrator
+TARGET_USES_DEVICE_SPECIFIC_VIBRATOR := true
+
 # Inherit from msm8937-common
 $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
@@ -102,6 +105,10 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
 
 # Wifi
 PRODUCT_PACKAGES += \
