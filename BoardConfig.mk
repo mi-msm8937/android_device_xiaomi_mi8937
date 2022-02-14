mi-msm8937/android_device_xiaomi_ulova

ulysse: init: Add libinit for variants handling
Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

BoardConfig.mk
@@ -12,6 +12,10 @@ DEVICE_PATH := device/xiaomi/ulysse
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := ulysse,ugglite,ugg
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_ulysse
+TARGET_RECOVERY_DEVICE_MODULES := libinit_ulysse
+
 # Kernel
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 

mi-msm8937/android_device_xiaomi_mi8937

BoardConfig.mk
@@ -12,6 +12,10 @@ DEVICE_PATH := device/xiaomi/ulysse
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := ulysse,ugglite,ugg
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_ulysse
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_ulysse
+
 # Kernel
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 